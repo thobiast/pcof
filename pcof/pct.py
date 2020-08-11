65edee3b9f4931484f5d4fb0535eab80d7a8d055
Thobias Salazar Trevisan <thobiast@gmail.com>
2020-08-11 13:42:44 -0300

    Move pct functions to its own module.
    Rename function pct_two_numbers to y_what_pct_of_x
    and add % char in the return string.

---
+++ pcof/pct.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Python Collection Of Functions.
+
+Package with collection of small useful functions.
+
+Percentage Calculator
+"""
+
+
+def y_what_pct_of_x(number1, number2, *, precision="2"):  # pragma: no cover
+    """
+    Calculate the percentage of number1 to number2.
+
+    Number1 is what percent of number2.
+
+    Arguments:
+        number1     (int): number
+        number2     (int): number
+
+    Keyword arguments (opt):
+        precision   (int): number of digits after the decimal point
+                           default is 2
+
+    Returns:
+        (str):  Pct value
+
+    Example:
+    >>> y_what_pct_of_x(30, 90)    # 30 is what percent of 90?
+    '33.33%'
+    >>> y_what_pct_of_x(30, 90, precision=0)
+    '33%'
+    >>> y_what_pct_of_x(30, 90, precision=4)
+    '33.3333%'
+    >>> y_what_pct_of_x(10, 50, precision=0) # 10 is what percent of 50?
+    '20%'
+    """
+    try:
+        num_pct = (number1 / number2) * 100
+        return "{0:.{prec}f}%".format(num_pct, prec=precision)
+    except ZeroDivisionError:
+        return "{0:.{prec}f}%".format(0, prec=precision)
+
+
+def x_pct_of_number(pct, number, *, precision="2"):  # pragma: no cover
+    """
+    Calculate what is the x% of a number.
+
+    Arguments:
+        pct        (int): percentage
+        number     (int): number
+
+    Keyword arguments (opt):
+        precision  (int): number of digits after the decimal point
+                          default is 2
+
+    Returns:
+        (str):  number
+
+    Exemple:
+    >>> x_pct_of_number(33.333, 90)     # what is 33.333% of 90?
+    '30.00'
+    >>> x_pct_of_number(40, 200)        # what is 40% of 200?
+    '80.00'
+    >>> x_pct_of_number(40.9, 200)      # what is 40.9% of 200?
+    '81.80'
+    >>> x_pct_of_number(40.9, 200, precision=4)
+    '81.8000'
+    >>> x_pct_of_number(40.9, 200, precision=0)
+    '82'
+    """
+    num = number * pct / 100
+    return "{0:.{prec}f}".format(num, prec=precision)
+
+
+# vim: ts=4
